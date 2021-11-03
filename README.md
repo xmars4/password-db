01b61dac4cf5efb11bcb156646b3cad5adf728e6
TruongPX <truongpx@forlife.vn>
2021-11-03 17:00:55 +0700

    Authenticate google spreadsheet api by service account

---
+++ README.md
@@ -0,0 +1,3 @@
+# Reference
+
+[Google Spreadsheet API](https://googleapis.dev/nodejs/googleapis/latest/sheets/index.html#getting-started)
--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-console.log("easy");
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+const http = require("http");
+const url = require("url");
+
+const { google } = require("googleapis");
+const people = google.people("v1");
+
+const keyPath = path.join(__dirname, "service-account-key.json");
+console.log(keyPath);
+
+if (!fs.existsSync(keyPath)) {
+    console.log("Missing service-account-key.json!");
+    console.log("You can follow this instruction to get the file: https://developers.google.com/identity/protocols/oauth2/service-account");
+}
+const scopes = ["https://www.googleapis.com/auth/contacts.readonly", "https://www.googleapis.com/auth/user.emails.read", "https://www.googleapis.com/auth/userinfo.profile"];
+
+const auth = new google.auth.GoogleAuth({
+    keyFile: keyPath,
+    scopes: scopes,
+});
+
+async function main() {
+    const authClient = await auth.getClient();
+
+    // obtain the current project Id
+    const project = await auth.getProjectId();
+
+    console.log(project);
+}
+
+main();
