a0a5328682466a6f908c8d83a8988fde7682c53a
TruongPX <truongpx@forlife.vn>
2021-11-04 10:43:53 +0700

    add permission to file

--- index.js
+++ index.js
@@ -6,16 +6,14 @@ const http = require("http");
 const url = require("url");
 
 const { google } = require("googleapis");
-const people = google.people("v1");
 
+// get service account file here: https://developers.google.com/identity/protocols/oauth2/service-account#creatinganaccount
 const keyPath = path.join(__dirname, "service-account-key.json");
-console.log(keyPath);
-
 if (!fs.existsSync(keyPath)) {
     console.log("Missing service-account-key.json!");
     console.log("You can follow this instruction to get the file: https://developers.google.com/identity/protocols/oauth2/service-account");
 }
-const scopes = ["https://www.googleapis.com/auth/contacts.readonly", "https://www.googleapis.com/auth/user.emails.read", "https://www.googleapis.com/auth/userinfo.profile"];
+const scopes = ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/spreadsheets"];
 
 const auth = new google.auth.GoogleAuth({
     keyFile: keyPath,
@@ -24,11 +22,43 @@ const auth = new google.auth.GoogleAuth({
 
 async function main() {
     const authClient = await auth.getClient();
+    const sheets = google.sheets({ version: "v4", auth: authClient });
+    const drive = google.drive({
+        version: "v2",
+        auth: authClient,
+    });
+    let sheet_id = "1GM9SIEKgDXk1kPPpBYR6cQIFtsU-ls9o1SKzPNkqBW0";
+    let sheet_file = await drive.files.get({ fileId: sheet_id });
+    console.log(sheet_file);
 
-    // obtain the current project Id
-    const project = await auth.getProjectId();
+    // create spreadsheet file
+    // const sheet = await sheets.spreadsheets.create({
+    //     requestBody: {
+    //         dataSourceSchedules: [],
+    //         dataSources: [],
+    //         developerMetadata: [],
+    //         namedRanges: [],
+    //         properties: {
+    //             title: "Password DB",
+    //         },
+    //         sheets: [],
+    //         spreadsheetId: "",
+    //         spreadsheetUrl: "",
+    //     },
+    // });
 
-    console.log(project);
+    // let file_id = sheet_file.spreadsheetId;
+    // let drive_file = await drive.files.get({ fileId: sheet_id });
+
+    // update file permission
+    // use this for test purpose only, in production, user don't need to see spreadsheet file
+    let permission_request = await drive.permissions.insert({
+        fileId: sheet_id,
+        resource: {
+            type: "anyone",
+            role: "writer",
+        },
+    });
 }
 
 main();
