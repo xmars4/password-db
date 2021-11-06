1d94858037b1f385c7bea5be88cdd31d093556b1
TruongPX <truongpx@forlife.vn>
2021-11-06 13:16:04 +0700

    connect to google docs and spreadsheet

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Reference
 
-[Google Spreadsheet API](https://googleapis.dev/nodejs/googleapis/latest/sheets/index.html#getting-started)
+[Google Docs API][(https://googleapis.dev/nodejs/googleapis/latest/sheets/index.html#getting-started](https://googleapis.dev/nodejs/googleapis/latest/docs/classes/Docs.html))
+
+## About
+
+We use google docs as a Database because it's free.
+The data is saved in json format
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const http = require("http");
 const url = require("url");
 
 const { google } = require("googleapis");
+let drive_obj = null;
+let docs_obj = null;
 
 // get service account file here: https://developers.google.com/identity/protocols/oauth2/service-account#creatinganaccount
 const keyPath = path.join(__dirname, "service-account-key.json");
@@ -13,37 +15,58 @@ if (!fs.existsSync(keyPath)) {
     console.log("Missing service-account-key.json!");
     console.log("You can follow this instruction to get the file: https://developers.google.com/identity/protocols/oauth2/service-account");
 }
-const scopes = ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/spreadsheets"];
+const scopes = ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/documents"];
 
 const auth = new google.auth.GoogleAuth({
     keyFile: keyPath,
     scopes: scopes,
 });
 
+async function read_data(file_id) {
+    let data = await docs_obj.documents.get({
+        documentId: file_id,
+    });
+    return data;
+}
+
+async function write_data(data) {}
+
+async function delete_data(data) {}
+
+async function update_permission(file_id, permission) {
+    // update file permission
+    // use this for test purpose only, in production, user don't need to see doc file
+    let permission_request = await drive_obj.permissions.insert({
+        fileId: file_id,
+        resource: permission,
+    });
+    return permission_request;
+}
+
 async function main() {
     const authClient = await auth.getClient();
-    const sheets = google.sheets({ version: "v4", auth: authClient });
-    const drive = google.drive({
+    docs_obj = google.docs({
+        version: "v1",
+        auth: authClient,
+    });
+    drive_obj = google.drive({
         version: "v2",
         auth: authClient,
     });
-    let sheet_id = "1GM9SIEKgDXk1kPPpBYR6cQIFtsU-ls9o1SKzPNkqBW0";
-    let sheet_file = await drive.files.get({ fileId: sheet_id });
-    console.log(sheet_file);
+    let doc_id = "1HHVPTaw0yHF8WPRWfWD7eJ0R4d2_EbfOoUkjcDXLOiw";
+    // let doc_file = await drive.files.get({ fileId: doc_id });
 
-    // create spreadsheet file
-    // const sheet = await sheets.spreadsheets.create({
+    // await update_permission(doc_id, {
+    //     type: "anyone",
+    //     role: "writer",
+    // });
+
+    let data = await read_data(doc_id);
+    console.log(data.data.body.content);
+    // create docs file
+    // const new_document = await docs.documents.create({
     //     requestBody: {
-    //         dataSourceSchedules: [],
-    //         dataSources: [],
-    //         developerMetadata: [],
-    //         namedRanges: [],
-    //         properties: {
-    //             title: "Password DB",
-    //         },
-    //         sheets: [],
-    //         spreadsheetId: "",
-    //         spreadsheetUrl: "",
+    //         title: "Password DB",
     //     },
     // });
 
@@ -52,13 +75,13 @@ async function main() {
 
     // update file permission
     // use this for test purpose only, in production, user don't need to see spreadsheet file
-    let permission_request = await drive.permissions.insert({
-        fileId: sheet_id,
-        resource: {
-            type: "anyone",
-            role: "writer",
-        },
-    });
+    // let permission_request = await drive.permissions.insert({
+    //     fileId: sheet_id,
+    //     resource: {
+    //         type: "anyone",
+    //         role: "writer",
+    //     },
+    // });
 }
 
 main();
